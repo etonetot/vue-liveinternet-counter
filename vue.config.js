cebae68115ea278841e60c5d3c5928c42afd9f38
Sergey Scob <C:\Users\scob\AppData\Roaming\The Bat!>
2020-09-01 17:34:12 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/'
-        : '/vue-input-calculator/',
+        : '/vue-liveinternet-counter/',
     outputDir: process.env.NODE_ENV === 'production'
         ? __dirname+'/lib'
         : __dirname+'/demo',
